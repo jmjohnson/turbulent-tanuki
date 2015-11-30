8e0bf241f7398ccbbf107d2dce895dc933f623c1
Jeff Johnson <energy.nexus@gmail.com>
2015-11-29 19:37:17 -0500

    Simple test spec to get started.

---
+++ test/mainSpec.js
@@ -0,0 +1,23 @@
+/**
+ * @fileoverview Suite for the primary js work.
+ */
+
+var MEETING_DESCRIPTION_WITH_NEWLINES =
+"Toll-free dial-in number (U.S. and Canada): (866) 243-6264i\n" +
+"Conference code: 915.607.3554\n" +
+"Leader PIN/Web Meeting password: 372954 \n\n" +
+"Toll-free dial-in number (U.S. and Canada):" +
+"(866) 243-6264\n\n" +
+
+"Conference code:\n" +
+"6502876775\n\n" +
+
+"Leader PIN\n" +
+"177500"
+;
+
+describe("Hello world", function() {
+  it("finds the conference code in the meeting description", function() {
+    console.log(CalendarService)
+  });
+});
