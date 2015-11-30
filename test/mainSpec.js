6412ea009fcf760d7c6111f82dfd15dfd4a19aa3
Jeff Johnson <energy.nexus@gmail.com>
2015-11-29 19:49:56 -0500

    Finally got a failing test set up!

--- test/mainSpec.js
+++ test/mainSpec.js
@@ -5,7 +5,9 @@
 var MEETING_DESCRIPTION_WITH_NEWLINES =
 "Toll-free dial-in number (U.S. and Canada): (866) 243-6264i\n" +
 "Conference code: 915.607.3554\n" +
-"Leader PIN/Web Meeting password: 372954 \n\n" +
+"Leader PIN/Web Meeting password: 372954 \n\n";
+
+var MEETING_DESCRIPTION_WITH_NEWLINES_2 =
 "Toll-free dial-in number (U.S. and Canada):" +
 "(866) 243-6264\n\n" +
 
@@ -18,6 +20,7 @@ var MEETING_DESCRIPTION_WITH_NEWLINES =
 
 describe("Hello world", function() {
   it("finds the conference code in the meeting description", function() {
-    console.log(CalendarService)
+    var result = CalendarService.prototype.meetingDescToConferenceCode(MEETING_DESCRIPTION_WITH_NEWLINES);
+    expect(result).toEqual("6502876775");
   });
 });
